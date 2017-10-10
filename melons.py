aeca2dd5e18106e46feef56bb0b8fdc5cd07e290
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-09 17:58:55 -0700

    Added other dictionaries and commented further steps

---
+++ melons.py
@@ -0,0 +1,51 @@
+melon_names = {
+    1: "Honeydew",
+    2: "Crenshaw",
+    3: "Crane",
+    4: "Casaba",
+    5: "Cantaloupe",
+}
+
+melon_prices = {
+    1: 0.99,
+    2: 2.00,
+    3: 2.50,
+    4: 2.50,
+    5: 0.99,
+}
+
+melon_seedlessness = {
+    1: True,
+    2: False,
+    3: False,
+    4: False,
+    5: False,
+}
+
+melon_flesh_color = {
+    1: "brown"
+    2: "None"
+    3: "green"
+    4: "pink"
+    5: "blue"
+}
+
+melon_rind_color = {
+    1: "peach"
+    2: "green"
+    3: "white"
+    4: "white"
+    5: "red"
+}
+
+melon_average_weight = {
+    1: 3.4
+    2: 2
+    3: 2.1
+    4: 1.5
+    5: 1
+}
+
+#step 1: #TODO: Save this file as a .txt file (melons.py)
+#Step 2: combine the data of this file into a dictionary with the key-value pairs (melon_info.py)
+#step 3: search by melon_name to display output in the format (in melons_info.py)
